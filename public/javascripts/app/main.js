2e0426e2fc312700b866a76517eeb0ef8d77a9e1
Allan Bettarel <allan.bettarel@gmail.com>
2013-08-05 00:58:11 -0400

    [FRONT] - App fixed - routes added

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -1,10 +1,19 @@
 require([
-	
+	"namespace",
 	"use!backbone",
+	"router"
+],
+function (JUGGLEVENT, Backbone, Router) {
 
-],function (Backbone) {
-
+	$(function() {
 		
+		var app = JUGGLEVENT.initApp();
+		var module = JUGGLEVENT.module("Views");
 
-	
+		app.router = new Router();
+
+		Backbone.history.start({ pushState: true, root: "/app/"});
+
+	});
+
 });
